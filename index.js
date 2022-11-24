1bff1a5e10b89d66e5ea5f3c4856dead0aa11a0c
klemonade <lemon.thanakrit@gmail.com>
2022-11-24 15:36:24 +0700

    guide

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 async function selectAll() {
-    const res = await (await fetch('http://localhost:3003/admin/selectall', {method: 'GET'})).json()
+    const res = await (await fetch('http://localhost:3003/admin/selectall', { method: 'GET' })).json()
     let list = `<ul>`
     res.data.forEach(element => {
         list += `<li>${element}</li?`
@@ -7,3 +7,21 @@ async function selectAll() {
     list += `</ul>`
     document.querySelector('#id').innerHTML = list
 }
+
+async function select(id) {
+    const res = await (await fetch('http://localhost:3003/admin/id', { method: 'GET', body: JSON.stringify({ admin_id: id }) })).json()
+    document.querySelector('#id').innerHTML = `<div>${res.data}</div>`
+}
+
+async function insert(admin) {
+    console.log(admin);
+    const res = await (await fetch('http://localhost:3003/admin/insert', {
+        method: 'POST', 
+        headers: {
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify({ admin })
+    })).json()
+    document.querySelector('#id').innerHTML = `<div>${res.data}</div>`
+}
+
