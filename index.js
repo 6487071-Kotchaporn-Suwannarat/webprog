d872e28cfa03908090a0f04b2ea65e1af947ba31
6487071 Kotchaporn Suwannarat <113087658+6487071-Kotchaporn-Suwannarat@users.noreply.github.com>
2022-11-24 15:47:54 +0700

    fix something

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-async function selectAll() {
-    const res = await (await fetch('http://localhost:3003/admin/selectall', {method: 'GET'})).json()
-    let list = `<ul>`
-    res.data.forEach(element => {
-        list += `<li>${element}</li?`
-    });
-    list += `</ul>`
-    document.querySelector('#id').innerHTML = list
-}
+// async function selectAll() {
+//     const res = await (await fetch('http://localhost:3003/admin/selectall', {method: 'GET'})).json()
+//     let list = `<ul>`
+//     res.data.forEach(element => {
+//         list += `<li>${element}</li?`
+//     });
+//     list += `</ul>`
+//     document.querySelector('#id').innerHTML = list
+// }
